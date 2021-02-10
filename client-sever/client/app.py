ab06058e7f15dfd0d37ed8e1615c4bcc5a42a654
timur <timur.alchin@flatstack.com>
2021-02-11 00:30:54 +0300

    Add regirect after registration

--- client-sever/client/app.py
+++ client-sever/client/app.py
@@ -5,6 +5,7 @@ import sys
 import json
 import asyncio
 import hashlib 
+from time import sleep
 
     
 
@@ -14,6 +15,8 @@ class ClientApp:
         self.port = port
         self.server_url = server_url
         self.window = Tk()
+        self.window.geometry('600x400')
+        self.window.resizable(width=False, height=False)
         
     def start(self):       
         self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -31,9 +34,9 @@ class ClientApp:
         #add if user is already loged in checking
         self.loginScreen()
     
-    def checkPassword(self, password, confirmPassword, errorLabel):
+    def checkPassword(self, password, confirmPassword, validationLabel):
         if password!=confirmPassword:
-            #errorLabel.config(text="Passwords are not the same")            
+            validationLabel.config(text="Passwords are not the same")            
             return False
         else:
             return True
@@ -42,7 +45,7 @@ class ClientApp:
         #self.window.withdraw()
         loginWindow =self.window #Toplevel(self.window)
         loginWindow.title('Login')
-        loginWindow.geometry('600x400')
+        #loginWindow.geometry('600x400')
 
         usernameLabel = Label(loginWindow, text="Username")  
         usernameLabel.place(anchor='center', relx =.5, rely=.1)  
@@ -63,7 +66,7 @@ class ClientApp:
         submitButton.place(anchor='center', relx =.5, rely=.6)       
        
 
-        registrationButton = Button(loginWindow, text="Don't have an account yet? ", command=lambda: self.registrationScreen(loginWindow))  
+        registrationButton = Button(loginWindow, text=f'Do not have \nan account yet?', command=lambda: self.registrationScreen(loginWindow))  
         registrationButton.place(anchor='center', relx =.5, rely=.7)
         
     def registrationScreen(self, parentWindow):
@@ -90,11 +93,15 @@ class ClientApp:
         confirmPasswordLabel.place(anchor='center', relx =.5, rely=.5)
         
         confirmPasswordField = Entry(registrationWindow,width=10,show="*")  
-        confirmPasswordField.place(anchor='center', relx =.5, rely=.55)   
+        confirmPasswordField.place(anchor='center', relx =.5, rely=.55)  
+
+        validationLabel = Label(registrationWindow, text="")
+        validationLabel.place(anchor='center', relx =.5, rely=.6) 
             
-        errorLabel = Label(registrationWindow, text="")
-        errorLabel.place(anchor='center', relx =.5, rely=.7)   
-        submitButton = Button(registrationWindow, text="Register", command=lambda: self.submitRegistration(usernameField.get(), passwordField.get(),confirmPasswordField.get(),errorLabel))  
+        responseLabel = Label(registrationWindow, text="")
+        responseLabel.place(anchor='center', relx =.5, rely=.7) 
+          
+        submitButton = Button(registrationWindow, text="Register", command=lambda: self.submitRegistration(usernameField.get(), passwordField.get(),confirmPasswordField.get(),responseLabel, validationLabel, registrationWindow, parentWindow))  
         submitButton.place(anchor='center', relx =.5, rely=.8)  
         
         goBackButton = Button(registrationWindow, text="Go back", command=lambda: self.goBack(parentWindow, registrationWindow))  
@@ -117,8 +124,8 @@ class ClientApp:
     
             
 
-    def submitRegistration(self, username, password,confirmPassword, errorLabel):
-        if not self.checkPassword(password,confirmPassword,errorLabel):
+    def submitRegistration(self, username, password,confirmPassword, responseLabel,validationLabel, currentWindow, previousWindow):
+        if not self.checkPassword(password,confirmPassword, validationLabel):
             return
         encodedPassword = self.encodeText(password)        
         user = {"username":username, "password":encodedPassword}
@@ -127,7 +134,10 @@ class ClientApp:
         self.client_sock.sendall(bytes(userData, encoding="utf-8"))
         data = self.client_sock.recv(1024)
         message = data.decode('utf-8')
-        errorLabel.config(text=message)
+        responseLabel.config(text=message)
+        if message == "User is added!":
+            previousWindow.deiconify()
+            currentWindow.withdraw()
         
         
 
